02a206ec35d272c2d0246cf4aed6a3019ecf5f0e
Aefvvges <torri.barbara4@gmail.com>
2024-04-12 21:45:44 -0300

    commit

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 $a1 = 'hola';
 $a2 = 'mundo';
 
-echo = 'la variable a1 es igual a ' . a1 . ' y la variable a2 es igual a ' . a2;
+echo ('la variable a1 es igual a ' . a1 . ' y la variable a2 es igual a ' . a2) ;
 ?>
